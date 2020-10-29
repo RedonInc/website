4541405397095b0609561dbc7dd2470dad99b423
Rishabh Singh Chauhan <rishabhc913@gmail.com>
2020-10-29 21:04:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.route('/')
+    .get( function (req, res) {
+        res.render('home');
+    })
+    .post(function (req, res){
+        res.redirect('/index')
+    })
+
+app.route('/index')
+    .get(function (req, res){
+        res.render('index')
+    })
+
+app.route('/tutorial')
+    .get(function (req, res) {
+        res.send("Tutorial")
+    })
+
+app.route('/resource')
+    .get(function (req, res) {
+        res.send("Resource")
+    })
+app.route('/lab')
+    .get(function (req, res) {
+        res.send("Labs")
+    })
+app.route('/about')
+    .get(function (req, res) {
+        res.send("About Us")
+    })
+
+app.listen(process.env.PORT || 3000, function () {
+        console.log("listening on port 3000")
+    })
+    
